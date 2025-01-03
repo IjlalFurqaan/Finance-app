cd9ee8f1704410548a23415a544800c7af374a56
Ijlal Furqaan <45873251+IjlalFurqaan@users.noreply.github.com>
2025-01-03 11:01:10 +0100

    pre final update

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -6,8 +6,7 @@ import FlexBetween from "@/components/FlexBetween";
 
 type Props = {};
 
-const Navbar = (props: Props) => {
-  
+const Navbar = (_props: Props) => {
   const { palette } = useTheme();
   const [selected, setSelected] = useState("dashboard");
   return (
--- server/index.js
+++ server/index.js
@@ -31,12 +31,13 @@ app.use("/transaction", transactionRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
-console.log("MongoDB URI:", process.env.MONGO_URI); // Debug log
-
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(async () => {
-    app.listen(PORT, () => console.log(`Server running on Port: ${PORT}`));
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /* ADD DATA ONE TIME ONLY OR AS NEEDED */
     // await mongoose.connection.db.dropDatabase();
@@ -44,4 +45,4 @@ mongoose
     // Product.insertMany(products);
     // Transaction.insertMany(transactions);
   })
-  .catch((error) => console.log(`${error.message} - Database connection failed.`));
+  .catch((error) => console.log(`${error} did not connect`));
